576330c7cc77dad26ba86bed184ef78cae55ac59
Daniel Tran <daniel@digitalcreators.com.au>
2021-04-08 12:16:07 +1000

    Added top margin to back button container to reduce tap area and reduce instances of interfering with the iOS system default app back switcher

--- src/ExNavigationBar.js
+++ src/ExNavigationBar.js
@@ -132,6 +132,7 @@ const buttonStyles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 8,
   },
   button: {
     resizeMode: 'contain',
